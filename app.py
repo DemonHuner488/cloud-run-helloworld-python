c171cdb8efeaa76d984668546f179f5b9fbd81a9
DemonHunter488 <camargoandres488@gmail.com>
2026-02-05 11:07:09 -0500

    Fix routes: homepage on / and hello on /hello

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/hello")
 def hello_world():
-    target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
+    target = os.environ.get("TARGET", "World")
+    return "Hello {}!\n".format(target)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
